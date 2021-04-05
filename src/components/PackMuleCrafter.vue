a2b5dcc3d01b781cca2f36fbcd0fe07dcebf4e0a
pabhd3 <pabhd3@gmail.com>
2021-04-04 21:07:05 -0500

    Added Additional Bootstrapping Completed 4/7 TODOs for MVP [Done] Clean up Bootstrap Jumbotron + Recipe/Material/Inventory overflow issue [Done] Add "Updated to vX.XX" notifier [Done] Change "Inventory Setups" from raw JSON --> Bootstrapped something [Done] Change <ul /> and <ol /> to something better

--- src/components/PackMuleCrafter.vue
+++ src/components/PackMuleCrafter.vue
@@ -1,65 +1,79 @@
 <template>
   <div class="jumbotron">
-    <!-- Anvil Tab Selector -->
-    <div class="row pl-3 mb-4" style="float:left;padding-right:100px">
-      <div class="form-group">
-        <h3>Anvil Tab Unlocked</h3>
-        <select v-model="anvilTab" class="custom-select" id="anvilSelector">
-          <option value="" selected>Select Anvil Tab</option>
-          <option value="anvil1">Anvil Tab 1</option>
-          <option value="anvil2">Anvil Tab 2</option>
-        </select>
-      </div>
-    </div>
-    <!-- Task Tier Selector -->
-    <div class="row pl-3 mb-4" style="float:left">
-      <div class="form-group">
-        <h3>Task Tier</h3>
-        <select v-model="taskTier" class="custom-select" id="tierSelector">
-          <option value="" selected>Select Task Tier</option>
-          <option value="tier1">Tier 1</option>
-          <option value="tier2">Tier 2</option>
-          <option value="tier3">Tier 3</option>
-          <option value="tier4">Tier 4</option>
-          <option value="tier5">Tier 5</option>
-          <option value="tier6">Tier 6</option>
-          <option value="tier7">Tier 7</option>
-          <option value="tier8">Tier 8</option>
-          <option value="tier9">Tier 9</option>
-          <option value="tier10">Tier 10</option>
-        </select>
+    <!-- Dropdown Selectors -->
+    <div class="container" id="selectors">
+      <div class="row">
+        <!-- Anvil Tab Selector -->
+        <div class="col-sm-5 pl-3 mb-4">
+          <div class="form-group">
+            <h3>Anvil Tab Unlocked</h3>
+            <select v-model="anvilTab" class="custom-select" id="anvilSelector">
+              <option value="" selected>Select Anvil Tab</option>
+              <option value="anvil1">Anvil Tab 1</option>
+              <option value="anvil2">Anvil Tab 2</option>
+            </select>
+          </div>
+        </div>
+        <!-- Task Tier Selector -->
+        <div class="col-sm-5 pl-3 mb-4">
+          <div class="form-group">
+            <h3>Task Tier</h3>
+            <select v-model="taskTier" class="custom-select" id="tierSelector">
+              <option value="" selected>Select Task Tier</option>
+              <option value="tier1">Tier 1 (3 Items)</option>
+              <option value="tier2">Tier 2 (8 Items)</option>
+              <option value="tier3">Tier 3 (14 Items)</option>
+              <option value="tier4">Tier 4 (24 Items)</option>
+              <option value="tier5">Tier 5 (34 Items)</option>
+              <option value="tier6">Tier 6 (45 Items)</option>
+              <option value="tier7">Tier 7 (55 Items)</option>
+              <option value="tier8">Tier 8 (70 Items)</option>
+              <option value="tier9">Tier 9 (90 Items)</option>
+              <option value="tier10">Tier 10 (110 Items)</option>
+            </select>
+          </div>
+        </div>
+        <!-- Game Version Note -->
+        <div class="col-sm-2 pl-3 mb-4">
+          <h3>Last Updated</h3>
+          <p>v1.13</p>
+          <p>Apr 04, 2021</p>
+        </div>
       </div>
     </div>
     <!-- Display Recommended -->
-    <div v-if="anvilTab && taskTier">
-      <div style="clear:both"/>
-      <!-- <div>{{ recommended }}</div> -->
-      <div style="float:left;width:30%;padding-right:5%">
-        <h4>Recipe List</h4>
-        <div id="recipeList" style="height:200px;overflow:auto">
-          <ol>
-            <li v-for="(recipe, index) in recommended.recipes" :key="`recipe-${index}`">
-              {{ recipe }}
-            </li>
-          </ol>
+    <div v-if="anvilTab && taskTier" class="container">
+      <div class="row">
+        <!-- Recipe + Materials -->
+        <div class="col-sm-4" id="recipesMaterials">
+          <h4>Recipe List</h4>
+          <div class="container" id="recipeList" style="height:200px;overflow:auto">
+            <div class="row" v-for="(recipe, index) in recommended.recipes" :key="`recipe-${index}`">
+              <div class="border-top border-bottom col-sm-2">{{ index+1 }}</div>
+              <div class="border-top border-bottom col-sm-10">{{ recipe }}</div>
+            </div>
+          </div>
+          <h4>Material List</h4>
+          <div class="container" id="materialList" style="height:200px;overflow:auto">
+            <div class="row" v-for="(material, index) in recommended.materials" :key="`material-${index}`">
+              <div class="border-top border-bottom col-sm-2" />
+              <div class="border-top border-bottom col-sm-10">{{ material }}</div>
+            </div>
+          </div>
         </div>
-        <h4>Material List</h4>
-        <div id="materialList" style="height:200px;overflow:auto">
-          <ul>
-            <li v-for="(material, index) in recommended.materials" :key="`material-${index}`">
-              {{ material }}
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div id="inventoryList">
-        <h4>Inventory Setups</h4>
-        <div style="width:65%;height:475px;float:left;overflow:auto">
-          <ol>
-            <li v-for="(inventory, index) in recommended.caps" :key="`inventory-${index}`">
-              {{ inventory }}
-            </li>
-          </ol>
+        <div class="col-sm-8" id="inventoryList">
+          <h4>Inventory Capacity Setups</h4>
+          <div class="container" style="height:475px;overflow:auto">
+            <div class="row" v-for="(inventory, index) in recommended.caps" :key="`inventory-${index}`">
+              <div class="border-top border-bottom col-sm"><h5>Slots: {{ inventory.slots }}</h5></div>
+              <div class="border-top border-bottom col-sm">Material:<br/>{{ inventory.material }}</div>
+              <div class="border-top border-bottom col-sm">Mining:<br/>{{ inventory.mining }}</div>
+              <div class="border-top border-bottom col-sm">Fish:<br/>{{ inventory.fish }}</div>
+              <div class="border-top border-bottom col-sm">Food:<br/>{{ inventory.food }}</div>
+              <div class="border-top border-bottom col-sm">Chopping:<br/>{{ inventory.choppin }}</div>
+              <div class="border-top border-bottom col-sm">Bug:<br/>{{ inventory.bug }}</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
