4d57f5cfebdfe9dc097d080cf18615d968ce2167
adapap <19696846+adapap@users.noreply.github.com>
2021-04-20 13:59:38 -0400

    fix: character selector broken

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import Home from "./pages/Home.vue";
 
 import "./styles/base.sass";
 import "./styles/progress.sass";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
 export default defineComponent({
   name: "App",
   components: {
