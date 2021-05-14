543ef3d2ee57f845d089af8e834607dcc231d7bf
Musimaniac <l.cacace1@gmail.com>
2021-05-14 22:33:22 +0200

    Improve UI, make responsive

--- src/pages/RecipeCalculator.vue
+++ src/pages/RecipeCalculator.vue
@@ -9,31 +9,34 @@
     </div>
   </div>
   <!-- Recipe + Quantity selectors -->
-  <div class="container text-light" id="selectors">
-    <div class="row">
-      <!-- Recipe Selector -->
-      <div class="col-sm-5 pl-3 mb-2">
-        <div class="input-group">
-          <select v-model="recipe" class="" id="recipeSelector">
+  <div class="row d-flex justify-content-between flex-wrap" id="selectors">
+    <!-- Recipe Selector -->
+    <div class="col d-flex flex-wrap mb-3">
+      <div class="col-auto mb-3 me-3">
+        <select v-model="recipe" class="" id="recipe-selector">
           <option value="" selected>Select Your Recipe</option>
           <option v-for="(recipe, id) in data" :key="id" :value="id">
             {{ recipe.name }}
           </option>
         </select>
-        </div>
       </div>
       <!-- Quantity Input -->
-      <div class="col-sm-5 pl-3 mb-2">
-        <label for="stamp-lvl" class="h5 m-2 ms-0">Quantity</label>
-        <input
-          v-model.number="quantity"
-          type="number"
-          min="1"
-          id="recipe-quantity"
-        />
+      <div class="col-auto">
+        <div class="input-group">
+          <span class="input-group-text">Quantity</span>
+          <input
+            v-model.number="quantity"
+            type="number"
+            min="1"
+            id="recipe-quantity"
+            class="form-control"
+          />
+        </div>
       </div>
       <!-- Game Version Note -->
-      <div class="col-sm-2 pl-3 mb-2">
+    </div>
+    <div class="col-auto text-light">
+      <div id="version-group" class="p-2">
         <h3>Last Updated</h3>
         <p>v1.14</p>
         <p>May 13, 2021</p>
@@ -41,7 +44,7 @@
     </div>
   </div>
   <!-- Display Materials -->
-  <div v-if="recipe && quantity" class="container text-light">
+  <div v-if="recipe && quantity" class="text-light">
     <h4>Materials</h4>
     <div
       class="row"
@@ -87,6 +90,14 @@ export default defineComponent({
       }
     );
     return { data, recipe, quantity, materials };
-  }
+  },
 });
 </script>
+
+<style lang="sass" scoped>
+@import '../styles/base.sass'
+
+#version-group
+  border: 2px solid $primary
+  border-radius: 0.25rem
+</style>
