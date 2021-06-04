53e852f205b08e948e10b0b1aa8e3adc73549d42
Thijs <vanherwijnenthijs98@gmail.com>
2021-06-04 16:02:43 +0200

    Cleaning up before pull request  * Reformated the file use spaces instead of tabs  * Added the version div  * TODO: Swap variable names of recipe and tab selectors 				 Make the recipe selector value empty when the tab selector changes

--- src/pages/RecipeCalculator.vue
+++ src/pages/RecipeCalculator.vue
@@ -8,56 +8,60 @@
       </p>
     </div>
   </div>
-  <!-- Recipe + Quantity selectors -->
+  <!-- Tab +  Recipe + Quantity selectors -->
   <div class="row d-flex justify-content-between flex-wrap" id="selectors">
     <div class="col d-flex flex-wrap mb-3">
-			<!-- Recipe Selector -->
+      <!-- Tab Selector (TODO: rename the variable, not sure why it breaks when i swap it with tab-->
       <div class="col-auto mb-3 me-3">
-        <select v-model="recipe" class="" id="recipe-selector">
-          <option value="" selected>Select the anvil tab</option>
-					<option v-for="(group, id) in data" :key="id" :value="id">
-            {{ id}}
-          </option>
+        <select v-model="recipe" class id="recipe-selector">
+          <option value selected>Select the anvil tab</option>
+          <option v-for="(group, id) in data" :key="id" :value="id">{{ id}}</option>
         </select>
       </div>
-			<div class="col-auto mb-3 me-3">
-        <select v-model="tab" class="" id="tab-selector">
-          <option value="" selected>Select Your Recipe</option>
-					<option v-for="(group, id) in data[recipe]" :key="id" :value="id">
-            {{ id}}
-          </option>
+      <!-- Recipe Selector -->
+      <div class="col-auto mb-3 me-3">
+        <select v-model="tab" class id="tab-selector">
+          <option value selected>Select Your Recipe</option>
+          <option v-for="(group, id) in data[recipe]" :key="id" :value="id">{{ id}}</option>
         </select>
-
       </div>
-		</div>
-		<div class="col-auto">
-			<div class="input-group">
-				<span class="input-group-text">Quantity</span>
-				<input
-					v-model.number="quantity"
-					type="number"
-					min="1"
-					id="recipe-quantity"
-					class="form-control"
-				/>
-			</div>
-		</div>
-		Version 1.20b
-		<div v-if="tab" class="text-light">
-			<h4>Materials</h4>
-			<div
-				class="row"
-				v-for="(material) in data[recipe][tab]['Materials']"
-				:key="`material-${index}`"
-			>
-				<div class="border-top border-bottom col-sm-2 align-right">
-					{{ (material[2] * parseInt(quantity)).toLocaleString() }}
-				</div>
-				<div class="border-top border-bottom col-sm-10 padded-start">
-				{{ material[1].padStart(material[1].length+material[0]*4)}} 
-				</div>
-			</div>
-		</div>
+    </div>
+    <div class="col-auto">
+      <div class="input-group">
+        <span class="input-group-text">Quantity</span>
+        <input
+          v-model.number="quantity"
+          type="number"
+          min="1"
+          id="recipe-quantity"
+          class="form-control"
+        />
+      </div>
+    </div>
+    <!-- Version -->
+    <div class="col-auto text-light">
+      <div id="version-group" class="p-2">
+        <h3>Last Updated</h3>
+        <p>v1.20b</p>
+        <p>June 4, 2021</p>
+      </div>
+    </div>
+  </div>
+  <!-- Materials -->
+  <div v-if="tab" class="text-light">
+    <h4>Materials</h4>
+    <div
+      class="row"
+      v-for="(material) in data[recipe][tab]['Materials']"
+      :key="`material-${index}`"
+    >
+      <div
+        class="border-top border-bottom col-sm-2 align-right"
+      >{{ (material[2] * parseInt(quantity)).toLocaleString() }}</div>
+      <div
+        class="border-top border-bottom col-sm-10 padded-start"
+      >{{ material[1].padStart(material[1].length+material[0]*4)}}</div>
+    </div>
   </div>
 </template>
 
@@ -97,6 +101,6 @@ export default defineComponent({
 @import '../styles/base.sass'
 
 #version-group
-  border: 2px solid $primary
-  border-radius: 0.25rem
+    border: 2px solid $primary
+    border-radius: 0.25rem
 </style>
