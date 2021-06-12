70ad3e72388b3dd079896bfeaf4f33e10304aaab
100mars <13767112+100mars@users.noreply.github.com>
2021-06-11 21:51:43 -0400

    Monster JSON Update

--- src/composables/SweetSpotWeight.ts
+++ src/composables/SweetSpotWeight.ts
@@ -1,16 +1,282 @@
 export const monsterWeight = {
-	"Green Mushroom" : { 
-		"None" : 7,
-		"Warrior" : 7,
-		"Archer" : 7,
-		"Mage" : 7,
-		"Journeyman" : 7
-	},
-	"Frog" : {
-		"None" : 21,
-		"Warrior" : 21,
-		"Archer" : 21,
-		"Mage" : 21,
-		"Journeyman": 21
-	}
-};
+  "Green Mushroom" : { 
+	"None" : 7,
+	"Warrior" : 7,
+	"Archer" : 7,
+	"Mage" : 7,
+	"Journeyman" : 7
+  },
+  "Frog" : {
+	"None" : 21,
+	"Warrior" : 21,
+	"Archer" : 21,
+	"Mage" : 21,
+	"Journeyman": 21
+  },
+  "Bored Bean": {
+	"None" : 16.5,
+	"Warrior" : 16.5,
+	"Archer" : 16.5,
+	"Mage" : 16.5,
+	"Journeyman" : 16.5
+  },
+  "Slime": {
+	"None" : 16.5,
+	"Warrior" : 16.5,
+	"Archer" : 16.5,
+	"Mage" : 16.5,
+	"Journeyman" : 16.5
+  },
+  "Baby Boa": {
+	"None" : 10.9,
+	"Warrior" : 10.9,
+	"Archer" : 10.9,
+	"Mage" : 10.9,
+	"Journeyman" : 10.9
+  },
+  "Carrotman": {
+	"None" : 7.75,
+	"Warrior" : 7.75,
+	"Archer" : 7.75,
+	"Mage" : 7.75,
+	"Journeyman" : 7.75 
+  },
+ "Red Mushroom": {
+	"None" : 5.25,
+	"Warrior" : 5.25,
+	"Archer" : 5.25,
+	"Mage" : 5.25,
+	"Journeyman" : 5.25
+  },
+  "Glublin": {
+	"None" : 13.25,
+	"Warrior" : 13.25,
+	"Archer" : 13.25,
+	"Mage" : 13.25,
+	"Journeyman" : 13.25 
+  },
+  "Wode Board": {
+	"None" : 10.1,
+	"Warrior" : 10.1,
+	"Archer" : 10.1,
+	"Mage" : 10.1,
+	"Journeyman" : 10.1
+  },
+  "Gigafrog": {
+	"None" : 8.5,
+	"Warrior" : 8.5,
+	"Archer" : 8.5,
+	"Mage" : 8.5,
+	"Journeyman" : 8.5
+  },
+  "Walking Stick": {
+	"None" : 20.3,
+	"Warrior" : 20.3,
+	"Archer" : 20.3,
+	"Mage" : 20.3,
+	"Journeyman" : 20.3
+  },
+  "Nutto": {
+	"None" : 17,
+	"Warrior" : 17,
+	"Archer" : 17,
+	"Mage" : 17,
+	"Journeyman" : 17
+  },
+  "Wood Mushroom": {
+	"None" : 5,
+	"Warrior" : 5,
+	"Archer" : 5,
+	"Mage" : 5,
+	"Journeyman" : 5
+  },
+  "Poop": {
+	"None" : 18.4,
+	"Warrior" : 18.4,
+	"Archer" : 18.4,
+	"Mage" : 18.4,
+	"Journeyman" : 18.4
+  },
+  "Rat": {
+	"None" : 17,
+	"Warrior" : 17,
+	"Archer" : 17,
+	"Mage" : 17,
+	"Journeyman" : 17
+  },
+  "Sandy Pot": {
+	"None" : 9.4,
+	"Warrior" : 9.4,
+	"Archer" : 9.4,
+	"Mage" : 9.4,
+	"Journeyman" : 9.4
+  },
+  "Mimic": {
+	"None" : 14.3,
+	"Warrior" : 14.3,
+	"Archer" : 14.3,
+	"Mage" : 14.3,
+	"Journeyman" : 14.3
+  },
+  "Crabcake": {
+	"None" : 11.2,
+	"Warrior" : 11.2,
+	"Archer" : 11.2,
+	"Mage" : 11.2,
+	"Journeyman" : 11.2
+  },
+  "Mafioso": {
+	"None" : 9,
+	"Warrior" : 9,
+	"Archer" : 9,
+	"Mage" : 9,
+	"Journeyman" : 9
+  },
+  "Sand Castle": {
+	"None" : 11.5,
+	"Warrior" : 11.5,
+	"Archer" : 11.5,
+	"Mage" : 11.5,
+	"Journeyman" : 11.5
+  },
+  "Pincermin": {
+	"None" : 9.5,
+	"Warrior" : 9.5,
+	"Archer" : 9.5,
+	"Mage" : 9.5,
+	"Journeyman" : 9.5
+  },
+  "Mashed Potato": {
+	"None" : 11.5,
+	"Warrior" : 11.5,
+	"Archer" : 11.5,
+	"Mage" : 11.5,
+	"Journeyman" : 11.5
+  },
+  "Tyson": {
+	"None" : 9.4,
+	"Warrior" : 9.4,
+	"Archer" : 9.4,
+	"Mage" : 9.4,
+	"Journeyman" : 9.4
+  },
+  "Moonmoon": {
+	"None" : 5.9,
+	"Warrior" : 5.9,
+	"Archer" : 5.9,
+	"Mage" : 5.9,
+	"Journeyman" : 5.9
+  },
+  "Sand Giant": {
+	"None" : 19.75,
+	"Warrior" : 19.75,
+	"Archer" : 19.75,
+	"Mage" : 19.75,
+	"Journeyman" : 19.75
+  },
+  "Snelbie": {
+	"None" : 3.8,
+	"Warrior" : 3.8,
+	"Archer" : 3.8,
+	"Mage" : 3.8,
+	"Journeyman" : 3.8
+  },
+  "Sheepie": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Frost Flake": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Sir Stache": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Bloque": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Mamooth": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Snowman": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Penguin": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Thermister": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Quenchie": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Cryosnake": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Bop Box": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+  "Neyeptune": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+ "Dedotated Ram": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  },
+   "Bloodbone": {
+	"None" : 0,
+	"Warrior" : 0,
+	"Archer" : 0,
+	"Mage" : 0,
+	"Journeyman" : 0
+  }
+}
