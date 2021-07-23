d0b75545a58701bc295ea6050d98541b53659018
adapap <19696846+adapap@users.noreply.github.com>
2021-07-23 19:41:38 -0400

    wip(cards): card page, ui incomplete

--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -1,51 +1,44 @@
 <template>
-  <div class="row">
-    <div>
-      <p class="h6 text-light bg-primary p-3 mt-3 mb-1 rounded">
-        This list shows possible cards sets grouped by usage. Cards are listed
-        from the most to the least useful, left to right.
-      </p>
-    </div>
-  </div>
-  <div class="p-3" id="card-wrapper">
+  <q-banner inline-actions>
+    Find which cards are best for a certain objective! Cards are grouped by
+    having direct bonuses (Choppin' EXP), indirect bonuses (Accuracy), or
+    additional benefit (Drop Chance).
+    <template v-slot:action>
+      <q-btn-dropdown outline label="Wiki">
+        <q-list separator>
+          <a v-for="[label, link] in wikiLinks" :key="label" :href="link">
+            <q-item clickable>
+              <q-item-section>{{ label }}</q-item-section>
+            </q-item>
+          </a>
+        </q-list>
+      </q-btn-dropdown>
+    </template>
+  </q-banner>
+  <div class="p-3">
     <div class="col-12" v-for="(cardset, i) in cardData" :key="i">
       <div class="h1 text-light">{{ cardset.title }}</div>
       <div class="row justify-content-start">
-        <div
-          class="col-4 border border-secondary rounded p-2 w-auto flex"
-          style="min-width: 150px"
-        >
+        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
           <span class="badge bg-secondary card-title">Core Cards</span>
-          <GameAsset
-            v-for="(card, c) in cardset.core"
-            :key="c"
-            :height="72"
+          <ICAsset
+            v-for="card in cardset.core"
             :image="Assets.CardImage(card)"
             :title="card.replace(/_/g, ' ')"
           />
         </div>
-        <div
-          class="col-4 border border-secondary rounded p-2 w-auto flex"
-          style="min-width: 150px"
-        >
+        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
           <span class="badge bg-secondary card-title">Useful Cards</span>
-          <GameAsset
-            v-for="(card, c) in cardset.useful"
-            :key="c"
-            :height="72"
+          <ICAsset
+            v-for="card in cardset.useful"
             :image="Assets.CardImage(card)"
             :title="card.replace(/_/g, ' ')"
           />
         </div>
-        <div
-          class="col-4 border border-secondary rounded p-2 w-auto flex"
-          style="min-width: 150px"
-        >
+        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
           <span class="badge bg-secondary card-title">Minimal Benefits</span>
-          <GameAsset
-            v-for="(card, c) in cardset.minimal"
-            :key="c"
-            :height="72"
+          <ICAsset
+            v-for="card in cardset.minimal"
             :image="Assets.CardImage(card)"
             :title="card.replace(/_/g, ' ')"
           />
@@ -62,6 +55,8 @@ import cardData from "~/data/card-sets.json";
 import ICAsset from "~/components/idleon-companion/IC-Asset.vue";
 import { Assets } from "~/composables/Utilities";
 
+const WikiLinks = new Map([["Cards", "https://idleon.info/wiki/Cards"]]);
+
 export default defineComponent({
   name: "Cards",
   components: {
@@ -71,6 +66,7 @@ export default defineComponent({
     return {
       Assets,
       cardData,
+      wikiLinks: WikiLinks,
     };
   },
 });
