595bd8d508f8fe00f2c365f5614241c8a9197eee
adapap <19696846+adapap@users.noreply.github.com>
2021-07-26 21:53:35 -0400

    feat(cards): redesign cards page

--- src/pages/Cards.vue
+++ src/pages/Cards.vue
@@ -15,37 +15,68 @@
       </q-btn-dropdown>
     </template>
   </q-banner>
-  <div class="p-3">
-    <div class="col-12" v-for="(cardset, i) in cardData" :key="i">
-      <div class="h1 text-light">{{ cardset.title }}</div>
-      <div class="row justify-content-start">
-        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
-          <span class="badge bg-secondary card-title">Core Cards</span>
-          <ICAsset
-            v-for="card in cardset.core"
-            :image="Assets.CardImage(card)"
-            :title="card.replace(/_/g, ' ')"
-          />
-        </div>
-        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
-          <span class="badge bg-secondary card-title">Useful Cards</span>
-          <ICAsset
-            v-for="card in cardset.useful"
-            :image="Assets.CardImage(card)"
-            :title="card.replace(/_/g, ' ')"
-          />
-        </div>
-        <div class="col-4 border border-secondary rounded p-2 w-auto flex">
-          <span class="badge bg-secondary card-title">Minimal Benefits</span>
-          <ICAsset
-            v-for="card in cardset.minimal"
-            :image="Assets.CardImage(card)"
-            :title="card.replace(/_/g, ' ')"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
+  <q-card class="m-4">
+    <q-card-section>
+      <div class="text-2xl">Card Sets</div>
+    </q-card-section>
+    <q-markup-table>
+      <thead class="text-right">
+        <tr>
+          <th><div class="text-lg">Goal</div></th>
+          <th><div class="text-lg">Core</div></th>
+          <th><div class="text-lg">Secondary</div></th>
+          <th><div class="text-lg">Extra</div></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="cardset in cardData" class="text-right">
+          <td>{{ cardset.title }}</td>
+          <td>
+            <ICAsset
+              v-for="(card, index) in cardset.core"
+              size="none"
+              :key="index"
+              :image="Assets.CardImage(card)"
+              :title="card.replace(/_/g, ' ')"
+              class="card-image"
+            >
+              <template #tooltip>
+                {{ card.replace(/_/g, " ") }}
+              </template>
+            </ICAsset>
+          </td>
+          <td>
+            <ICAsset
+              v-for="(card, index) in cardset.useful"
+              size="none"
+              :key="index"
+              :image="Assets.CardImage(card)"
+              :title="card.replace(/_/g, ' ')"
+              class="card-image"
+            >
+              <template #tooltip>
+                {{ card.replace(/_/g, " ") }}
+              </template>
+            </ICAsset>
+          </td>
+          <td>
+            <ICAsset
+              v-for="(card, index) in cardset.minimal"
+              size="none"
+              :key="index"
+              :image="Assets.CardImage(card)"
+              :title="card.replace(/_/g, ' ')"
+              class="card-image"
+            >
+              <template #tooltip>
+                {{ card.replace(/_/g, " ") }}
+              </template>
+            </ICAsset>
+          </td>
+        </tr>
+      </tbody>
+    </q-markup-table>
+  </q-card>
 </template>
 
 <script lang="ts">
@@ -73,10 +104,8 @@ export default defineComponent({
 </script>
 
 <style lang="sass" scoped>
-.card-text
-  white-space: pre-line
-
-.card-title
-  position: absolute
-  opacity: 90%
+.card-image
+  display: inline-block
+  height: 64px
+  width: 48px
 </style>
