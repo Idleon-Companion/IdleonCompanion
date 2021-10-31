e7ceaf4ce45a8ff6565a0cf4b11bbc43544efcd7
desophos <1887450+desophos@users.noreply.github.com>
2021-10-31 01:24:15 -0500

    Change vials to match game data

--- src/composables/Alchemy.ts
+++ src/composables/Alchemy.ts
@@ -61,7 +61,7 @@ export const Vials: Vial[] = [
     effect: "Water Droplet max capacity",
   },
   {
-    name: "Dieter Drenk",
+    name: "Dieter Drink",
     roll: 40,
     material: "Bean_Slices",
     base: 1,
@@ -75,7 +75,7 @@ export const Vials: Vial[] = [
     effect: "% Class EXP when converting",
   },
   {
-    name: "Skinny O Cal",
+    name: "Skinny 0 Cal",
     roll: 60,
     material: "Snake_Skin",
     base: 2.5,
@@ -103,14 +103,14 @@ export const Vials: Vial[] = [
     effect: "% Card Drop rate",
   },
   {
-    name: "Tea with Pea",
+    name: "Tea With Pea",
     roll: 75,
     material: "Potty_Rolls",
     base: 3,
     effect: "% Liquid Nitrogen max capacity",
   },
   {
-    name: "Gold Guzzler",
+    name: "Gold Guzzle",
     roll: 83,
     material: "Gold_Ore",
     base: 1,
@@ -131,7 +131,7 @@ export const Vials: Vial[] = [
     effect: " chance for a kill to count double",
   },
   {
-    name: "Fly in my Drink",
+    name: "Fly In My Drink",
     roll: 87,
     material: "Fly",
     base: 3,
@@ -180,7 +180,7 @@ export const Vials: Vial[] = [
     effect: "% higher Shiny Critter chance.",
   },
   {
-    name: "Slippy Soul",
+    name: "Sippy Soul",
     roll: 75,
     material: "Forest Soul",
     base: 1,
@@ -292,7 +292,7 @@ export const Vials: Vial[] = [
     effect: "Talent Point for Tab 3",
   },
   {
-    name: "40 40 Purity",
+    name: "40-40 Purity",
     roll: 98,
     material: "Contact Lense",
     base: 3,
