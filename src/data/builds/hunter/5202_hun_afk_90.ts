20da2b48b2f97c0378d608bbdff502f90f40e8c7
MacroWaves <96925556+MacroWaves@users.noreply.github.com>
2022-02-18 23:42:18 +0100

    Update 5202_hun_afk_90.ts

--- src/data/builds/hunter/5202_hun_afk_90.ts
+++ src/data/builds/hunter/5202_hun_afk_90.ts
@@ -5,47 +5,50 @@ import { Build } from "~/composables/Builds";
 export const build: Build = {
   title: "Hunter / AFK (~90)",
   notes:
-    "Leveling up active skills directly increase AFK kills/hr, so we level them past 1.",
-  version: "1.23",
+    "Active Skills act as a multiplyer to your total dmg for afk. lvl 25 on all of them should be enough to reach dmg cap. Put more points into attack talents and "Have Another!" if you are using Prayers that increase mob hp.",
+  version: "1.40b",
   level: 90,
   tabs: [
     {
       skills: {
-        "1": "*2",
+        "1": "*1",
+        "4": "*1",
         "5": "100+",
-        "6": "*1",
-        "7": "*2",
+        "6": "1+",
+        "7": "1+",
         "10": "100+",
         "12": "10",
         "14": "100+",
-        "15": "*2",
+        "15": "100+",
       },
       comment:
-        '*1 = Boost STR to have as much accuracy as you need for the best AFK gains. Usually this will be the farthest monster you can reach without dying. If you\'re low on defense, by this point equipping food to stay alive is normal and expected. \n"Idle Shooting" to 100 for 13% AFK gain rate. \n Max "Gilded Sword" & "Sharpened Axe", 10 into "Featherweight" for MS. \n *2 = Excess points may go here.',
+        '*1 = Boost STR to have as much accuracy as you need for the best AFK gains. *1 If you need Survivability.',
     },
     {
       skills: {
-        "1": "70",
-        "2": "70",
-        "4": "1",
-        "5": "50",
-        "6": "20",
+        "1": "25",
+        "2": "25",
+        "4": "50+",
+        "5": "25",
+        "6": "100+",
+        "7": "100+",
         "8": "100+",
+        "9": "1+",
       },
       comment:
         'Level "Polymer Limbs" to max, active damage skills to 70, and then value point into "Strafe" and activate it before going offline. Afterwards, level up "Have Another" to 50 & "Veins of the Infernal" to 20. \n Excess points into "Veins of the Infernal" or "Have Another".',
     },
     {
       skills: {
-        "1": "50",
-        "2": "50",
-        "5": "30",
-        "6": "50",
-        "11": "*1",
+        "1": "25",
+        "2": "25",
+        "5": "25",
+        "6": "100+",
+        "11": "160",
         "15": "*1",
       },
       comment:
-        'Level up "360 Noscope" and "Bear Trap" to 50. \n Then raise "Have another... Again" to 30 and "Looty Mcshooty" to 50 or so. \n "*1 = "Eagle Eye" should be taken here for trapping. Excess points can be placed into "Previous Points".',
+        'Max "Eagle Eye" for trapping. With a high enough skill lvl and "Poison Froge Vial" you can collect more than 100% critters despite the skill saying otherwise. \n Excess points can be placed into "Previous Points".',
     },
   ],
   class: Class.Hunter,
