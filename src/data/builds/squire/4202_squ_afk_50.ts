96878f3ac33712cdc329c7cb44b99340bf9c36b2
MacroWaves <96925556+MacroWaves@users.noreply.github.com>
2022-03-08 14:38:20 +0100

    Update 4202_squ_afk_50.ts

--- src/data/builds/squire/4202_squ_afk_50.ts
+++ src/data/builds/squire/4202_squ_afk_50.ts
@@ -17,7 +17,7 @@ export const build: Build = {
         "14": "100",
       },
       comment:
-        '*1 = Boost WIS to have as much accuracy as you need for the best AFK gains. Usually this will be the farthest monster you can reach without dying. If you\'re low on defense, by this point equipping food to stay alive is normal and expected.\n"Idle Brawling" to 50 for 10% AFK gain rate.\nPriority switches to Gilded sword > Meatshank > Sharpened axe',
+        '*1 = Boost WIS to have as much accuracy as you need for the best AFK gains. Usually this will be the farthest monster you can reach without dying. If you\'re low on defense, by this point equipping food to stay alive is normal and expected.\nPriority switches to Gilded sword > Meatshank > Sharpened axe',
     },
     {
       skills: {
