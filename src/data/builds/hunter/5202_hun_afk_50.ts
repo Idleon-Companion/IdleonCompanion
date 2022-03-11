4172e6e93e54ebb7d7d35936b086702dbdfdc1b8
MacroWaves <96925556+MacroWaves@users.noreply.github.com>
2022-03-11 15:01:24 +0100

    Update 5202_hun_afk_50.ts

--- src/data/builds/hunter/5202_hun_afk_50.ts
+++ src/data/builds/hunter/5202_hun_afk_50.ts
@@ -5,15 +5,15 @@ import { Build } from "~/composables/Builds";
 export const build: Build = {
   title: "Hunter / AFK (~50)",
   notes:
-    "Active Skills act as a multiplyer to your total dmg for afk. \n How many points you need depends on your "Attacks on Simmer" Star Talent and "All for Kill" bubble. If you got spare talent resets, reset your build at your fighting area and lvl attack talents last till you do not gain any more kills per hour.",
+    "Active Skills act as a multiplyer to your total dmg for afk.",
   version: "1.40b",
   level: 50,
   tabs: [
     {
       skills: {
-        "5": "80",
+        "5": "25+",
         "6": "*1",
-        "10": "80",
+        "10": "25+",
         "14": "50",
       },
       comment:
@@ -24,22 +24,20 @@ export const build: Build = {
         "1": "25",
         "2": "25",
         "4": "1",
-        "5": "20",
         "6": "50",
         "8": "50",
       },
       comment:
-        'Level "Polymer Limbs" to 50, active damage skills to 25, then value point into "Strafe" and activate it before going offline. Afterwards, level up "Have Another" to 20 & "Veins of the Infernal" to 50 if you can afford. Raise your Smithing lvl by produceing 2/3x thread. ',
+        'Level "Polymer Limbs" to 50, active damage skills to 25, then value point into "Strafe" and activate it before going offline. Afterwards, level up "Veins of the Infernal" to 50 if you can afford. Raise your Smithing lvl by produceing 2/3x thread. ',
     },
     {
       skills: {
         "1": "30",
         "2": "30",
-        "5": "20",
-        "6": "20",
+        "6": "20+",
       },
       comment:
-        'Level up "360 Noscope" and "Bear Trap" to 30. \n Then raise "Have another... Again" and "Looty Mcshooty" to 20.',
+        'Level up "360 Noscope" and "Bear Trap" to 30. \n Then raise "Looty Mcshooty" to 20+.',
     },
   ],
   class: Class.Hunter,
