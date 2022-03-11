f1cb65596cf4bcb7ea50f334188f8ea4f20ce409
MacroWaves <96925556+MacroWaves@users.noreply.github.com>
2022-03-11 14:44:05 +0100

    Update 4105_barb_afk_50.ts

--- src/data/builds/barbarian/4105_barb_afk_50.ts
+++ src/data/builds/barbarian/4105_barb_afk_50.ts
@@ -4,31 +4,30 @@ import { Build } from "~/composables/Builds";
 
 export const build: Build = {
   title: "Barbarian / AFK (~50)",
-  notes: "Active Skills act as a multiplyer to your total dmg for afk. lvl 25 on all of them should be enough to reach dmg cap. \n How many points you need depends on your "Attacks on Simmer" Star Talent and "All for Kill" bubble. If you got spare talent resets, reset your build at your fighting area and lvl attack talents last till you do not gain any more kills per hour.  ",
+  notes: "Active Skills act as a multiplyer to your total dmg for afk. lvl 25 on all of them should be enough to reach dmg cap.  ",
   version: "1.40b",
   level: 50,
   tabs: [
     {
       skills: {
-        "5": "50",
+        "5": "25+",
         "8": "*1",
-        "10": "75",
-        "12": "50",
+        "10": "25+",
+        "12": "25+",
         "14": "50",
       },
       comment:
-        '*1 = Boost WIS to have as much accuracy as you need for the best AFK gains.\n"Idle Brawling" to 50 for 10% AFK gain rate.\nPriority switches to Gilded sword > Meatshank > Sharpened axe',
+        '*1 = Boost WIS to have as much accuracy as you need for the best AFK gains.\n"Idle Brawling" to 50 for 10% AFK gain rate.',
     },
     {
       skills: {
         "1": "25",
         "2": "25",
-        "4": "20",
         "5": "1",
         "8": "60",
       },
       comment:
-        'Level "Carry A Big Stick" to 60, then your active damage skills to 25. Afterwards, bring "Double-Strike" up to 20. Excess points can be used to level "Carry A Big Stick" past 60.\n 1 point into "Firmly Grasp It" gives extra STR.',
+        'Level "Carry A Big Stick" to 60, then your active damage skills to 25. \n 1 point into "Firmly Grasp It" gives extra STR. \n Excess points can be used to level "Carry A Big Stick" past 60.',
     },
     {
       skills: {
