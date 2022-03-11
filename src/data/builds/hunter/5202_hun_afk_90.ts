344e20724a2e3dee9adf13235b4edc95a48a396d
MacroWaves <96925556+MacroWaves@users.noreply.github.com>
2022-03-11 15:10:30 +0100

    Update 5202_hun_afk_90.ts

--- src/data/builds/hunter/5202_hun_afk_90.ts
+++ src/data/builds/hunter/5202_hun_afk_90.ts
@@ -13,13 +13,13 @@ export const build: Build = {
       skills: {
         "1": "*1",
         "4": "*1",
-        "5": "100+",
+        "5": "75+",
         "6": "1+",
         "7": "1+",
-        "10": "100+",
+        "10": "75+",
         "12": "10",
         "14": "100+",
-        "15": "100+",
+        "15": "75",
       },
       comment:
         '*1 = Boost STR to have as much accuracy as you need for the best AFK gains. *1 If you need Survivability.',
@@ -29,14 +29,13 @@ export const build: Build = {
         "1": "25",
         "2": "25",
         "4": "50+",
-        "5": "25",
-        "6": "100+",
-        "7": "100+",
+        "6": "50+",
+        "7": "1+",
         "8": "100+",
         "9": "1+",
       },
       comment:
-        'Level "Polymer Limbs" to max, active damage skills to 70, and then value point into "Strafe" and activate it before going offline. Afterwards, level up "Have Another" to 50 & "Veins of the Infernal" to 20. \n Excess points into "Veins of the Infernal" or "Have Another".',
+        'Level "Polymer Limbs" to max, active damage skills to 70, and then value point into "Strafe" and activate it before going offline. \n Excess points into "Veins of the Infernal"',
     },
     {
       skills: {
@@ -44,7 +43,7 @@ export const build: Build = {
         "2": "25",
         "5": "25",
         "6": "100+",
-        "11": "160",
+        "11": "140+",
         "15": "*1",
       },
       comment:
